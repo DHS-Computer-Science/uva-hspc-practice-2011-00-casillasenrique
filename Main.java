c8a75524b2f965078104426a8268b9652256a44f
casillasenrique <casillasenrique@live.com>
2016-01-06 08:27:55 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 //TODO: <name>
 //TODO: <date>
 //Practice 2011 - 00: Hello Judge
-
+import java.util.Scanner;
 public class  {
   public static void main(String[] args) {
-
+    Scanner input = new Scanner(System.in);
+   int num = input.nextInt();
+   for(int i = 0; i<num; i++)
+   {
+      System.out.println("Hello World, Judge " + (i + 1) + "!");
+   }
     System.exit(0);
   }
 }
